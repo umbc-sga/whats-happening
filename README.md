684d5eea58497393f2236152aa45d2b39d5db121
Rees Draminski <rees.draminski@gmail.com>
2020-01-05 12:15:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# umbc-generator
+Create new abbreviations for UMBC.
---
+++ app.js
@@ -0,0 +1,3 @@
+$.get("https://my3.my.umbc.edu/api/v0/events.xml", function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
+});
